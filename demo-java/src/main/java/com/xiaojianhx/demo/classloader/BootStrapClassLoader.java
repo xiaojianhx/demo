fb52fdbf4692198b125f246c24ff59585cd32676
wanchao.gao@adinall.com <wanchao.gao@adinall.com>
2018-02-07 18:04:13 +0800

    add classloader test

---
+++ demo-java/src/main/java/com/xiaojianhx/demo/classloader/BootStrapClassLoader.java
@@ -0,0 +1,34 @@
+package com.xiaojianhx.demo.classloader;
+
+import java.net.URL;
+
+import com.xiaojianhx.utils.CheckUtils;
+
+public class BootStrapClassLoader {
+
+    public static void main(String[] args) {
+
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+
+        for (URL url : urls) {
+            System.out.println(url);
+        }
+        System.out.println("---------------------------------");
+        System.out.println(System.getProperty("sun.boot.class.path"));
+
+        ClassLoader loader = BootStrapClassLoader.class.getClassLoader();
+
+        System.out.println("---------------------------------");
+
+        while (true) {
+
+            System.out.println(loader);
+
+            if (CheckUtils.isNull(loader)) {
+                break;
+            }
+            loader = loader.getParent();
+
+        }
+    }
+}

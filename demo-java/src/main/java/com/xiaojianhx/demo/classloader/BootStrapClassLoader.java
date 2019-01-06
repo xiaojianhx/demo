6b6184f3905858286fa8366880277e7aaab91df1
wanchao.gao@adinall.com <wanchao.gao@adinall.com>
2019-01-06 16:39:11 +0800

    update jdk to 10

--- demo-java/src/main/java/com/xiaojianhx/demo/classloader/BootStrapClassLoader.java
+++ demo-java/src/main/java/com/xiaojianhx/demo/classloader/BootStrapClassLoader.java
@@ -1,34 +1,30 @@
 package com.xiaojianhx.demo.classloader;
 
-import java.net.URL;
-
-import com.xiaojianhx.utils.CheckUtils;
-
 public class BootStrapClassLoader {
 
-    public static void main(String[] args) {
-
-        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
-
-        for (URL url : urls) {
-            System.out.println(url);
-        }
-        System.out.println("---------------------------------");
-        System.out.println(System.getProperty("sun.boot.class.path"));
-
-        ClassLoader loader = BootStrapClassLoader.class.getClassLoader();
-
-        System.out.println("---------------------------------");
-
-        while (true) {
-
-            System.out.println(loader);
-
-            if (CheckUtils.isNull(loader)) {
-                break;
-            }
-            loader = loader.getParent();
-
-        }
-    }
+    // public static void main(String[] args) {
+    //
+    // URL[] urls = Launcher.getBootstrapClassPath().getURLs();
+    //
+    // for (URL url : urls) {
+    // System.out.println(url);
+    // }
+    // System.out.println("---------------------------------");
+    // System.out.println(System.getProperty("sun.boot.class.path"));
+    //
+    // ClassLoader loader = BootStrapClassLoader.class.getClassLoader();
+    //
+    // System.out.println("---------------------------------");
+    //
+    // while (true) {
+    //
+    // System.out.println(loader);
+    //
+    // if (CheckUtils.isNull(loader)) {
+    // break;
+    // }
+    // loader = loader.getParent();
+    //
+    // }
+    // }
 }

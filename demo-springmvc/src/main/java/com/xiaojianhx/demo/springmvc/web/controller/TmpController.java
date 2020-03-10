4ab0c706ca6661b6f82e115007df867fd9ca3d87
xiaojianhx@163.com <xiaojianhx@163.com>
2020-03-10 17:30:32 +0800

    springmvc

--- demo-springmvc/src/main/java/com/xiaojianhx/demo/springmvc/web/controller/TmpController.java
+++ demo-springmvc/src/main/java/com/xiaojianhx/demo/springmvc/web/controller/TmpController.java
@@ -1,48 +1,19 @@
 package com.xiaojianhx.demo.springmvc.web.controller;
 
-import java.io.File;
-import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TmpController {
 
-    @RequestMapping(value = "/upload")
-    public String upload(HttpServletRequest request, ModelMap model) {
-
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
-
-        if (multipartResolver.isMultipart(request)) {
-
-            // 将request变成多部分request
-            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
-
-            // 获取multiRequest 中所有的文件名
-            Iterator iter = multiRequest.getFileNames();
-
-            while (iter.hasNext()) {
-                // 一次遍历所有文件
-                MultipartFile file = multiRequest.getFile(iter.next().toString());
-                if (file != null) {
-                    String path = "/tmp/" + file.getOriginalFilename();
-                    try {
-                        file.transferTo(new File(path));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    model.addAttribute("fileUrl", "/tmp/" + file.getOriginalFilename());
-                }
-            }
-        }
-
-        return "upload_result";
+    @ResponseBody
+    @RequestMapping(value = "/a/get")
+    public Map get(HttpServletRequest request) {
+        return request.getParameterMap();
     }
 }

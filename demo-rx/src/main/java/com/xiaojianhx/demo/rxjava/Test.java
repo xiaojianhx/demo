8febe1359002aa86ef64e0d550221bd8bb938242
xiaojianhx@163.com <xiaojianhx@163.com>
2021-02-03 19:26:38 +0800

    update

--- demo-rx/src/main/java/com/xiaojianhx/demo/rxjava/Test.java
+++ demo-rx/src/main/java/com/xiaojianhx/demo/rxjava/Test.java
@@ -1,43 +1,69 @@
 package com.xiaojianhx.demo.rxjava;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class Test {
+
+    private static Logger logger = LogManager.getLogger(Test.class);
 
     public static void main(String[] args) {
 
         var observable = Observable.create(new ObservableOnSubscribe<Integer>() {
 
             public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+
+                logger.info("emitter:{}", 1);
                 e.onNext(1);
+
+                logger.info("emitter:{}", 2);
                 e.onNext(2);
+
+                logger.info("emitter:{}", 3);
                 e.onNext(3);
+
+                logger.info("emitter:{}", 4);
                 e.onNext(4);
+
+                Thread.sleep(3000);
+
+                logger.info("emitter:{}", 5);
+                e.onNext(5);
             }
         });
 
         var observer = new Observer<Integer>() {
 
             public void onSubscribe(Disposable d) {
+                logger.info("Disposable:{}", d);
             }
 
             public void onNext(Integer integer) {
-                System.out.println(integer);
+                logger.info(" onNext:{}", integer);
             }
 
             public void onError(Throwable e) {
-                System.out.println("onError : value : " + e.getMessage());
+                logger.info("onError:{}", e);
             }
 
             public void onComplete() {
-                System.out.println("onComplete");
+                logger.info("onComplete");
             }
         };
 
-        observable.subscribe(observer);
+        observable.subscribeOn(Schedulers.io()).observeOn(Schedulers.single()).subscribe(observer);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
     }
 }
